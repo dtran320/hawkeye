5f734d90912d70aee0cda28d8fd6d48bf01d46ed
David Tran <dtran320@gmail.com>
2016-11-05 14:42:18 -0700

    Add homepage and login form

--- hawkeye/api.py
+++ hawkeye/api.py
@@ -4,21 +4,32 @@ from flask import render_template
 
 # Routes
 
+
 @app.route("/")
 def homepage():
-    return '''
+    return render_template('homepage.html')
+    '''
     <ol>
         <li><a href="/report-bullying">report bullying</a></li>
         <li><a href="/view-bullying-reports">view reports</a></li>
     </ol>
     '''
+
+@app.route('/students/login/')
+def students_login():
+    return render_template('students-login.html')
+
+@app.route('/counselors/login/')
+def counselors_login():
+    return render_template('counselors-login.html')
+
 @app.route("/report-bullying")
 def report_bullying():
     return render_template('report-bullying.html')
 
 @app.route("/view-bullying-reports")
 def view_report_bullying():
-    return render_template('view-bullying-reports.html', reports=reports) 
+    return render_template('view-bullying-reports.html', reports=reports)
 
 @app.route("/api/submit-report", methods=['POST'])
 def handle_report_submit():
