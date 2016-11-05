cbdd563cd97149d0dce54430e94a04c976d24e50
Geoff <geoff616@gmail.com>
2016-11-05 13:54:33 -0700

    wire up reply form

--- hawkeye/api.py
+++ hawkeye/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 from flask import render_template
 import random
@@ -20,7 +20,7 @@ def report_bullying():
     return render_template('report-bullying.html', user_id=user_id, form_id=form_id)
 
 @app.route("/view-bullying-reports")
-def view_report_bullying():
+def view_bullying_reports():
     return render_template('view-bullying-reports.html', reports=reports) 
 
 @app.route("/api/submit-report", methods=['POST'])
@@ -42,8 +42,20 @@ def handle_report_submit():
 @app.route("/api/submit-reply", methods=['POST'])
 def handle_reply_submit():
     data = request.form.to_dict()
-    print data
-    return 'reply sent'
+    append_message_to_report(data)
+    return redirect(url_for('view_bullying_reports'))
+
+## Helpers
+def append_message_to_report(data):
+    for x in range(len(reports)):
+        if reports[x]['report_id'] == data['report_id']:
+            print 'in this if'
+            reports[x]['report_messages'].append({
+                'sender': data['user_name'],
+                'text': data['text']
+            })
+    print 'new report data'
+    print reports
 
 # State
 reports = []
